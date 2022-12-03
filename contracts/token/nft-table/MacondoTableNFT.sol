2422c2c4967114ef8345fe4c877176eef0efddcd
0x00 pluto <zhipeng@66plat.com>
2022-12-03 09:23:56 +0800

    change upgrade to uups

--- contracts/token/nft-table/MacondoTableNFT.sol
+++ contracts/token/nft-table/MacondoTableNFT.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URISto
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 import "../../core/nft-store/INFTStoreItem.sol";
 
@@ -17,10 +18,12 @@ contract MacondoTableNFT is
     ERC721URIStorageUpgradeable,
     PausableUpgradeable,
     AccessControlUpgradeable,
-    INFTStoreItem
+    INFTStoreItem,
+    UUPSUpgradeable
 {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -33,10 +36,12 @@ contract MacondoTableNFT is
         __ERC721URIStorage_init();
         __Pausable_init();
         __AccessControl_init();
+        __UUPSUpgradeable_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -103,4 +108,10 @@ contract MacondoTableNFT is
     {
         return super.supportsInterface(interfaceId);
     }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(UPGRADER_ROLE)
+    {}
 }
--- contracts/token/token-MCD/MacondoMCD.sol
+++ contracts/token/token-MCD/MacondoMCD.sol
@@ -6,15 +6,18 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20Burnable
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract MacondoMCD is
     Initializable,
     ERC20Upgradeable,
     ERC20BurnableUpgradeable,
     PausableUpgradeable,
-    AccessControlUpgradeable
+    AccessControlUpgradeable,
+    UUPSUpgradeable
 {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -26,10 +29,12 @@ contract MacondoMCD is
         __ERC20Burnable_init();
         __Pausable_init();
         __AccessControl_init();
+        __UUPSUpgradeable_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
-        _mint(msg.sender, 6000000000 * 10**decimals());
+        _grantRole(UPGRADER_ROLE, msg.sender);
+        _mint(msg.sender, 1000000000 * 10**decimals());
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -47,4 +52,10 @@ contract MacondoMCD is
     ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
     }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(UPGRADER_ROLE)
+    {}
 }
