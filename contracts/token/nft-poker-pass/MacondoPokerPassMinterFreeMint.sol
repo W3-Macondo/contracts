5d9214857f0fd961b1beb7fc4affa4fce1538108
0x00 pluto <0x00pluto@gmail.com>
2023-01-09 15:06:04 +0800

    add freemint contract

---
+++ contracts/token/nft-poker-pass/MacondoPokerPassMinterFreeMint.sol
@@ -0,0 +1,192 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
+
+import "../../core/nft-store/NFTStore.sol";
+import "../../core/nft-store/NFTStoreSellerIncrease.sol";
+
+contract MacondoPokerPassMinterFreeMint is
+    Initializable,
+    PausableUpgradeable,
+    AccessControlUpgradeable,
+    ReentrancyGuardUpgradeable,
+    NFTStore,
+    NFTStoreSellerIncrease,
+    UUPSUpgradeable
+{
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant SALE_MANAGE_ROLE = keccak256("SALE_MANAGE_ROLE");
+
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    CountersUpgradeable.Counter private _nonce;
+
+    // withdraw address
+    address internal withdrawAddress;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        INFTStoreItem _tokenContract,
+        address payable _withdrawAddress
+    ) public initializer {
+        __Pausable_init();
+        __AccessControl_init();
+        __ReentrancyGuard_init();
+        __UUPSUpgradeable_init();
+        __NFTMinterBlindBox_init(_tokenContract);
+        __NFTStoreSellerIncrease_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(SALE_MANAGE_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+
+        //check withdraw address
+        require(_withdrawAddress != address(0), "withdraw address is zero");
+        withdrawAddress = _withdrawAddress;
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(UPGRADER_ROLE)
+    {}
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function _saleBefore(
+        address to,
+        uint256 tokenId,
+        string memory uri,
+        uint256 price
+    ) internal override whenNotPaused {
+        //Limit each address to purchase only once
+        _guardLimitSoldCountByAddress(to, 1);
+        super._saleBefore(to, tokenId, uri, price);
+    }
+
+    function setSaleConfig(
+        uint256 _salePeroiod,
+        uint256 _salePrice,
+        uint256 _saleStartTime,
+        uint256 _saleEndTime,
+        uint256 _totalSupply
+    ) external onlyRole(SALE_MANAGE_ROLE) {
+        _setSaleDefaultConfig(
+            _salePeroiod,
+            _salePrice,
+            _saleStartTime,
+            _saleEndTime,
+            _totalSupply
+        );
+    }
+
+    function setSaleConfigPrice(uint256 _salePrice)
+        external
+        onlyRole(SALE_MANAGE_ROLE)
+    {
+        _setSaleConfigPrice(_salePrice);
+    }
+
+    function setSaleConfigPeriod(
+        uint256 _salePeriod,
+        uint256 _saleStartTime,
+        uint256 _saleEndTime
+    ) external onlyRole(SALE_MANAGE_ROLE) {
+        _setSaleConfigPeriod(_salePeriod);
+        _setSaleConfigSaleTime(_saleStartTime, _saleEndTime);
+    }
+
+    function setTotalSupply(uint256 _totalSupply)
+        external
+        onlyRole(SALE_MANAGE_ROLE)
+    {
+        _setTotalSupply(_totalSupply);
+    }
+
+    function addTotalSupply(uint256 amount)
+        external
+        onlyRole(SALE_MANAGE_ROLE)
+    {
+        _addTotalSupply(amount);
+    }
+
+    function resetSoldCount() external onlyRole(SALE_MANAGE_ROLE) {
+        _setSoldCount(0);
+    }
+
+    /**
+     * initial token id to start
+     * also initial token id counter
+     */
+    function setInitialTokenId(uint256 _initialTokenId)
+        external
+        onlyRole(SALE_MANAGE_ROLE)
+    {
+        _setInitialTokenId(_initialTokenId);
+    }
+
+    function setWithdrawAddress(address payable _withdrawAddress)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        require(_withdrawAddress != address(0), "withdraw address is zero");
+        withdrawAddress = _withdrawAddress;
+    }
+
+    function withdraw() external nonReentrant onlyRole(SALE_MANAGE_ROLE) {
+        _withdraw(withdrawAddress);
+    }
+
+    function getNonce() public view returns (uint256) {
+        return _nonce.current();
+    }
+
+    function recoverSigner(bytes32 hash, bytes memory signature)
+        public
+        pure
+        returns (address)
+    {
+        bytes32 ethSign = ECDSAUpgradeable.toEthSignedMessageHash(hash);
+        return ECDSAUpgradeable.recover(ethSign, signature);
+    }
+
+    //freeMint
+    function freeMint(address _to, bytes memory signature)
+        external
+        payable
+        nonReentrant
+    {
+        address to = _to;
+        require(to != address(0), "to address is zero");
+
+        uint256 currentNonce = getNonce();
+        _nonce.increment();
+
+        address signer = recoverSigner(
+            keccak256(abi.encodePacked(to, currentNonce)),
+            signature
+        );
+
+        _checkRole(SALE_MANAGE_ROLE, signer);
+
+        _saleFreeWithIncreaseTokenId(to);
+    }
+}
